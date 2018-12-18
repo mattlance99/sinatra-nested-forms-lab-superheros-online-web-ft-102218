44fddeb739d29bd20b4d538a11065af20b5f9b20
Matthew Cohen <matt.cohen182@gmail.com>
2018-12-18 00:04:59 +0000

    Automatically backed up by Learn

---
+++ app/models/super_hero.rb
@@ -0,0 +1,19 @@
+require 'pry'
+
+class Super_hero
+  attr_reader :name, :power, :bio
+ 
+ SUPERHEROES = []
+ 
+  def initialize(params)
+    @name = params[:name]
+    @power = params[:power]
+    @bio = params[:bio]
+    SUPERHEROES << self
+  end
+ 
+  def self.all
+    SUPERHEROES
+  end
+ 
+end
